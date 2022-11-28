c1727f3bc9a6d423a3d088a9cd88e553b7215d28
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2022-11-28 09:18:29 +0530

    Create trendingSlice.js

---
+++ netflix-ui/src/store/reducers/slices/trendingSlice.js
@@ -0,0 +1,52 @@
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter,
+} from "@reduxjs/toolkit";
+import axios from "baseAxios";
+
+export const trendingAdapter = createEntityAdapter();
+
+export const fetchTrending = createAsyncThunk(
+  "trendingSlice/fetchTrending",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(
+        `trending/all/day?api_key=334a51625e2d99a2cb4f30205a19b196`
+      );
+      return response.data.results;
+    } catch (error) {
+      if (!error.response) {
+        throw error;
+      }
+
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const trendingSlice = createSlice({
+  name: "trending",
+  initialState: trendingAdapter.getInitialState({ error: null }),
+  reducers: {},
+  extraReducers: {
+    [fetchTrending.fulfilled]: (state, action) => {
+      trendingAdapter.upsertMany(state, action.payload);
+    },
+
+    [fetchTrending.rejected]: (state, action) => {
+      if (action.payload) {
+        state.error = action.payload.status_message;
+      } else {
+        state.error = action.error;
+      }
+    },
+  },
+});
+
+export const { selectAll: selectAllTrendingVideos } =
+  trendingAdapter.getSelectors((state) => state.trending);
+
+export const selectTrendingError = (state) => state.trending.error;
+
+export default trendingSlice.reducer;
